1864cd5491558931bf73aeaa3f9c18cd562f25e3
inuyashafff <190454594@qq.com>
2018-08-27 13:18:34 -0400

    code

---
+++ src/entity/Item.java
@@ -0,0 +1,163 @@
+package entity;
+
+import java.util.Set;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Item {
+	private String itemId;
+	private String name;
+	private double rating;
+	private String address;
+	private Set<String> categories;
+	private String imageUrl;
+	private String url;
+	private double distance;
+	
+	/**
+	 * This is a builder pattern in Java.
+	 */
+	//必须是private,不然itembuilder就没法用了
+	//用builder是为了不给某些参数设默认值
+	private Item(ItemBuilder builder) {
+		this.itemId = builder.itemId;
+		this.name = builder.name;
+		this.rating = builder.rating;
+		this.address = builder.address;
+		this.categories = builder.categories;
+		this.imageUrl = builder.imageUrl;
+		this.url = builder.url;
+		this.distance = builder.distance;
+	}
+
+	
+	
+	public JSONObject toJSONObject() {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("item_id", itemId);
+			obj.put("name", name);
+			obj.put("rating", rating);
+			obj.put("address", address);
+			obj.put("categories", new JSONArray(categories));
+			obj.put("image_url", imageUrl);
+			obj.put("url", url);
+			obj.put("distance", distance);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return obj;
+	}
+
+	
+	public String getItemId() {
+		return itemId;
+	}
+	public String getName() {
+		return name;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public Set<String> getCategories() {
+		return categories;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public double getDistance() {
+		return distance;
+	}
+	//为了创建item这个类，用了这个不用先实例化，因为itembuilber实例化需要item
+	//必须用内嵌类，否则不能调用item的构造函数
+	public static class ItemBuilder {
+		private String itemId;
+		public ItemBuilder setItemId(String itemId) {
+			this.itemId = itemId;
+			return this;
+		}
+		public ItemBuilder setName(String name) {
+			this.name = name;
+			return this;
+		}
+		public ItemBuilder setRating(double rating) {
+			this.rating = rating;
+			return this;
+		}
+		public ItemBuilder setAddress(String address) {
+			this.address = address;
+			return this;
+		}
+		public ItemBuilder setCategories(Set<String> categories) {
+			this.categories = categories;
+			return this;
+		}
+		public ItemBuilder setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+			return this;
+		}
+		public ItemBuilder setUrl(String url) {
+			this.url = url;
+			return this;
+		}
+		//方便级联
+		public ItemBuilder setDistance(double distance) {
+			this.distance = distance;
+			return this;
+		}
+		private String name;
+		private double rating;
+		private String address;
+		private Set<String> categories;
+		private String imageUrl;
+		private String url;
+		private double distance;
+		
+		public Item build() {
+			return new Item(this);
+		}
+
+		
+	}
+	public static void main() {
+		Item item = new Item.ItemBuilder().setAddress("123").setUrl("oo").build();
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((itemId == null) ? 0 : itemId.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (itemId == null) {
+			if (other.itemId != null)
+				return false;
+		} else if (!itemId.equals(other.itemId))
+			return false;
+		return true;
+	}
+
+}
